dd1320ab7e294eca5af1ae00b715b74fefe639da
philipp <philipp.armingeon@googlemail.com>
2021-12-23 05:06:35 +0100

    Added further improvements to day 5 part one

--- day5/day5.go
+++ day5/day5.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Point struct {
-	X, Y string
+	X, Y  int
+	count int
 }
 
 type Line struct {
@@ -16,7 +18,12 @@ type Line struct {
 }
 
 func NewPoint() *Point {
-	p := Point{X: "", Y: ""}
+	p := Point{X: 0, Y: 0, count: 0}
+	return &p
+}
+
+func InitPoint(x, y int) *Point {
+	p := Point{X: x, Y: y, count: 0}
 	return &p
 }
 
@@ -67,32 +74,83 @@ func parseNumber(line string) []string {
 	return result
 }
 
+func findMax(lines []Line) (int, int) {
+	max_x := 0
+	max_y := 0
+	for _, l := range lines {
+		if l.start.X > max_x {
+			max_x = l.start.X
+		} else if l.start.Y > max_y {
+			max_y = l.start.Y
+		} else if l.end.X > max_x {
+			max_x = l.end.X
+		} else if l.end.Y > max_y {
+			max_y = l.end.Y
+		}
+	}
+	return max_x, max_y
+}
+
 // parse input to generate the field of hydrothermal vents
 func GetLines(input []string) []Line {
 	var lines []Line
+	var err error
 	for _, line := range input {
-		fmt.Println("LINE: ", line)
+		// fmt.Println("LINE: ", line)
 		l := NewLine()
 		p := NewPoint()
 
 		numbers := parseNumber(line)
-		fmt.Println("Numbers: ", numbers)
-		p.X = numbers[0]
-		p.Y = numbers[1]
-		fmt.Println("CURRENT POINT: ", p)
+		// fmt.Println("Numbers: ", numbers)
+		p.X, err = strconv.Atoi(numbers[0])
+		checkError(err)
+		p.Y, err = strconv.Atoi(numbers[1])
+		checkError(err)
+		// fmt.Println("CURRENT POINT: ", p)
 		l.start = *p
-		fmt.Println("CURRENT LINE: ", l)
+		// fmt.Println("CURRENT LINE: ", l)
 
-		p.X = numbers[2]
-		p.Y = numbers[3]
-		fmt.Println("CURRENT POINT: ", p)
+		p.X, err = strconv.Atoi(numbers[2])
+		checkError(err)
+		p.Y, err = strconv.Atoi(numbers[3])
+		checkError(err)
+		// fmt.Println("CURRENT POINT: ", p)
 		l.end = *p
-		fmt.Println("CURRENT LINE: ", l)
+		// fmt.Println("CURRENT LINE: ", l)
 		lines = append(lines, *l)
 	}
 	return lines
 }
 
-func CalcOverlap(lines []Line) int {
+func Initialize(lines []Line) [][]Point {
+	var board [][]Point
+	var horizontal []Point
+	MAX_X, MAX_Y := findMax(lines)
+	for i := 0; i <= MAX_Y; i++ {
+		for j := 0; j <= MAX_X; j++ {
+			p := InitPoint(j, i)
+			horizontal = append(horizontal, *p)
+		}
+		board = append(board, horizontal)
+		horizontal = []Point{}
+	}
+	return board
+}
+
+func CalcOverlap(lines []Line, board [][]Point) int {
+	//MAX := findMax(lines)
+	for _, l := range lines {
+		for _, line := range board {
+			// ignore lines which are diagonal
+			if l.start.X == l.end.X {
+				fmt.Println("VERTICAL: ", l)
+				fmt.Println("b", line)
+			} else if l.start.Y == l.end.Y {
+				fmt.Println("HORIZONTAL: ", l)
+			} else {
+				continue
+			}
+		}
+	}
 	return 0
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -12,11 +12,15 @@ func TestPartOne(t *testing.T) {
 		input := d5.ReadFile("test.txt")
 		got := d5.GetLines(input)
 		fmt.Println("GOT: ", got)
+		board := d5.Initialize(got)
+		fmt.Println("BOARD: ")
+		fmt.Println(board)
+		_ = d5.CalcOverlap(got, board)
 	})
 
 	t.Run("real input part one", func(t *testing.T) {
-		input := d5.ReadFile("input.txt")
-		got := d5.GetLines(input)
-		fmt.Println("GOT: ", got)
+		// input := d5.ReadFile("input.txt")
+		// got := d5.GetLines(input)
+		// fmt.Println("GOT: ", got)
 	})
 }
